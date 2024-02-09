50a5dd57b2d98d052964062a098e20f17f591c77
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-02-09 15:02:00 +0100

    feat: refactor hill cipher script

--- TTM4135-applied-cryptography-and-network-security/practical-assignment/hill_cipher.py
+++ TTM4135-applied-cryptography-and-network-security/practical-assignment/hill_cipher.py
@@ -1,66 +1,100 @@
 from sympy import Matrix
 import sys
 
-# Formula:
-# C = KP => K = CP^-1
-
-# Using 0.txt
-with open('ciphertexts/0.txt', 'r') as file:
-    ciphertext = file.read().strip()
-converted_ciphertext = ciphertext.replace(',', '0').replace('.', '1').replace('-', '2')
-
-# Determine if the ciphertext should be printed (use --p to print)
-print_ciphertext = '--p' in sys.argv
-
-if print_ciphertext:
-    print(f'Converted ciphertext: {converted_ciphertext}')
-    print()
-
+# Define alphabet and dictionaries for converting characters to numbers and vice versa
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012'
 char_to_num = {char: idx for idx, char in enumerate(alphabet)}
 num_to_char = {idx: char for idx, char in enumerate(alphabet)}
 
-def calculate_key(TH, HE):
+def calculate_key(p, c):
+    """Calcuates the key given a plaintext matrix that matches a ciphertext matrix."""
+
+    # Formula:
+    # C = KP => K = CP^-1
+
     # Plaintext matrix
-    p = Matrix([
-        [char_to_num['T'], char_to_num['H']],
-        [char_to_num['H'], char_to_num['E']]
+    p_num = Matrix([
+        [char_to_num[str(p[0, 0])], char_to_num[str(p[0, 1])]],
+        [char_to_num[str(p[1, 0])], char_to_num[str(p[1, 1])]]
     ])
 
     # Ciphertext matrix
-    c = Matrix([
-        [char_to_num[TH[0]], char_to_num[HE[0]]],
-        [char_to_num[TH[1]], char_to_num[HE[1]]]
+    c_num = Matrix([
+        [char_to_num[str(c[0, 0])], char_to_num[str(c[0, 1])]],
+        [char_to_num[str(c[1, 0])], char_to_num[str(c[1, 1])]]
     ])
 
     # Inverse plaintext matrix
-    p_inv = p.inv_mod(29)
+    p_inv = p_num.inv_mod(29)
 
     # Key matrix
-    k_tmp = c * p_inv
-    k = k_tmp.applyfunc(lambda x: x % 29)
+    k = (c_num * p_inv).applyfunc(lambda x: x % 29)
     
     return k
 
-# TH / HE for 0.txt
-TH = [['A', 'I'], ['A', 'I'], ['X', 'Z'], ['X', 'Z'], ['J', 'H'], ['J', 'H']]
-HE = [['X', 'Z'], ['J', 'H'], ['A', 'I'], ['J', 'H'], ['A', 'I'], ['X', 'Z']]
+def print_keys(p, c_possibilities):
+    """Prints the keys for all possible ciphertexts given a plaintext matrix and a list of ciphertext matrices."""
+    for index, c in enumerate(c_possibilities):
+        key = calculate_key(p, c)
+        print(f'K_{index + 1}: {key}')
 
-# TH / HE for 1.txt
-# TH = [['Z', 'W'], ['Z', 'W'], ['I', 'Z'], ['I', 'Z'], ['S', 'C'], ['S', 'C']]
-# HE = [['I', 'Z'], ['S', 'C'], ['Z', 'W'], ['S', 'C'], ['Z', 'W'], ['I', 'Z']]
+def run(file_path, p, c_possibilities):
+    # Read file
+    with open(file_path, 'r') as file:
+        ciphertext = file.read().strip()
+        converted_ciphertext = ciphertext.replace(',', '0').replace('.', '1').replace('-', '2')
 
-# TH / HE for 2.txt
-# TH = [['R', 'A'], ['Q', 'R']]
-# HE = [['Q', 'R'], ['R', 'A']]
+    # Print ciphertext (use --p flag to print)
+    print_ciphertext = '--p' in sys.argv
+    if print_ciphertext:
+        print('Converted ciphertext:')
+        print(converted_ciphertext)
+        print()
 
-# TH / HE for 3.txt
-# TH = [['K', 'L'], ['K', 'L'], ['G', 'K'], ['G', 'K'], ['L', 'P'], ['L', 'P']]
-# HE = [['G', 'K'], ['L', 'P'], ['K', 'L'], ['L', 'P'], ['K', 'L'], ['G', 'K']]
+    # Calculate keys
+    print_keys(p, c_possibilities)
 
-for i in range(len(TH)):
-    key = calculate_key(TH[i], HE[i])
+#########################################
+# Configure file path, plaintext matrix #
+# and all possible ciphertexts          #
+#########################################
 
-    print(f'TH = {TH[i]} and HE = {HE[i]}')
-    print(f'K: {key}')
-    print()
+# Text file
+file_path = 'ciphertexts/0.txt'
+
+# Most common bigrams in English
+p = Matrix([
+    ['T', 'H'],
+    ['H', 'E']
+])
+
+# All possible ciphertexts for the given plaintext 
+# (derived from frequency analysis on the ciphertext)
+c_possibilities = [
+    Matrix([
+        ['A', 'X'],
+        ['I', 'Z']
+    ]),
+    Matrix([
+        ['A', 'J'],
+        ['I', 'H']
+    ]),
+    Matrix([
+        ['X', 'A'],
+        ['Z', 'I']
+    ]),
+    Matrix([
+        ['X', 'J'],
+        ['Z', 'H']
+    ]),
+    Matrix([
+        ['J', 'A'],
+        ['H', 'I']
+    ]),
+    Matrix([
+        ['J', 'X'],
+        ['H', 'Z']
+    ])
+]
+
+run(file_path, p, c_possibilities)
